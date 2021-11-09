91601ab324b238adbd66bec01446479dd4cf891d
JirongKim <munja__@naver.com>
2021-11-10 00:21:10 +0900

    FEAT:Adding Image switch

--- app/src/main/java/com/hyun222/tistory/runningclub/MainActivity.java
+++ app/src/main/java/com/hyun222/tistory/runningclub/MainActivity.java
@@ -3,12 +3,37 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
+
+    Button m_btn;
+    ImageView imageView;
+    boolean i = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        m_btn = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+
+        m_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (i==true){
+                    imageView.setImageResource(R.drawable.bbangdol_laying);
+                    i=false;
+                }
+                else
+                {
+                    imageView.setImageResource(R.drawable.bbangdol_seating);
+                    i=true;
+                }
+            }
+        });
     }
 }
