dc1db04e9d0bc79139425cf855896b5665eef92e
Jirong <munja__@naver.com>
2021-11-24 00:43:58 +0900

    Running Club Homework. Random number Generator.

--- app/src/main/java/com/hyun222/tistory/runningclub/MainActivity.java
+++ app/src/main/java/com/hyun222/tistory/runningclub/MainActivity.java
@@ -5,38 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
  public class MainActivity extends AppCompatActivity {
 
     Button m_btn;
-    ImageView imageView;
-    int i = 0;
+    TextView m_text[] = new TextView[6];
+    int arr_textview_id[] = {R.id.textView1, R.id.textView2, R.id.textView3, R.id.textView4, R.id.textView5, R.id.textView6};
+    Integer arr_random_num[] = new Integer[6];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        for(int i = 0; i <arr_textview_id.length; i++){
+            final int index;
+            index = i;
+            m_text[index] = findViewById(arr_textview_id[index]);
+        }
+
         m_btn = findViewById(R.id.button);
-        imageView = findViewById(R.id.imageView);
 
         m_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (i==0){
-                    imageView.setImageResource(R.drawable.bbangdol_laying);
-                    i++;
-                }
-                else if(i==1)
-                {
-                    imageView.setImageResource(R.drawable.bbangdol_seating);
-                    i++;
-                }
-                else if(i==2)
-                {
-                    imageView.setImageResource(R.drawable.bbangdol_standing);
-                    i=0;
+                Random random = new Random();
+                for (int i = 0; i < arr_random_num.length; i++){
+                    arr_random_num[i] = random.nextInt(45) + 1;
+                    m_text[i].setText(Integer.toString(arr_random_num[i]));
                 }
             }
         });
